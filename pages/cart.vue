32be5a5016e0ac16b2b482826ab9a7ec706a3190
박민우 <hahahoho5915@danawa.com>
2022-02-23 16:25:12 +0900

    feat: store setting & add cart page
     - store 세팅
     - 카트페이지 추가

---
+++ pages/cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>카트 페이지입니다</h1>
+    <div>
+      <ul>
+        <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id">
+          {{ cartItem }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -11,7 +11,7 @@
       <div class="side-panel">
         <p class="name">{{ product.name }}</p>
         <p class="price">{{ product.price }}</p>
-<!--        <button type="button" @click="addToCart">Add to Cart</button>-->
+        <button type="button" @click="addToCart">Add to Cart</button>
       </div>
     </div>
   </div>
@@ -26,6 +26,13 @@ export default {
     const res = await fetchProductById(params.id)
     const product = res.data
     return {product}
+  },
+  methods: {
+    addToCart() {
+      console.log(1111)
+      this.$store.commit('addCartItem', this.product)
+      this.$router.push('/cart')
+    }
   }
 }
 </script>
